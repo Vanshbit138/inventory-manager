c4f99dcdcdc6341e3100aaa111b7d41a17c52bf3
Vanshbit138 <vanshjaiswal@bitcot.com>
2025-08-04 19:01:11 +0530

    Remove old folder after renaming project directory

--- Week-3/main.py
+++
@@ -1,9 +0,0 @@
-from inventory_manager.core import Inventory
-
-def main():
-    inventory = Inventory()
-    inventory.load_from_csv("data/products.csv")
-    inventory.generate_report()
-
-if __name__ == "__main__":
-    main()

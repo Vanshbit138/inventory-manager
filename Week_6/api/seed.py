f4fd666d3df1cb9769ff25b0275502a7d376ee74
Vanshbit138 <vanshjaiswal@bitcot.com>
2025-08-20 16:37:46 +0530

    Removed previous code

--- Week_6/api/seed.py
+++ Week_6/api/seed.py
@@ -1,30 +1,3 @@
-# import csv
-# from . import create_app
-# from .db import db
-# from .models import Product
-
-# app = create_app()
-
-
-# def seed_db():
-#     with app.app_context():
-#         db.create_all()  # Create tables if not exist
-#         with open("Week6/inventory.csv") as f:
-#             reader = csv.DictReader(f)
-#             for row in reader:
-#                 product = Product(
-#                     name=row["name"],
-#                     price=float(row["price"]),
-#                     quantity=int(row["quantity"]),
-#                 )
-#                 db.session.add(product)
-#             db.session.commit()
-#         print("Database seeded!")
-
-
-# if __name__ == "__main__":
-#     seed_db()
-
 import csv
 import os
 from typing import Optional
